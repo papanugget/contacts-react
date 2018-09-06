fe074f8efb4ae4451b47a4d8ed6de19b08c9f53e
papanugget <evilgenius04@gmail.com>
2018-09-06 08:38:20 -0400

    starting from scratch

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts;
-
-    return (
-      <ul>
-        {people.map(person => (
-          <li key={person.name}>{person.name}</li>
-        ))}
-      </ul>
-    );
-  }
-}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <ContactList
-          contacts={[
-            { name: 'Eddard' },
-            { name: 'Cersei' },
-            { name: 'Robert' },
-            { name: 'Arya' }
-          ]}
-        />
-        <ContactList
-          contacts={[{ name: 'Sansa' }, { name: 'Theon' }, { name: 'Robb' }]}
-        />
-      </div>
-    );
-  }
+    render() {
+        return ( < div >
+            Hello World
+
+            <
+            /div >
+        )
+    }
 }
 
 export default App;
