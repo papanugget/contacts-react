b22f540a2a7579505744765ad01a8ece05433a16
papanugget <evilgenius04@gmail.com>
2018-09-07 18:27:06 -0400

    add contact to server functionality

--- src/App.js
+++ src/App.js
@@ -41,6 +41,13 @@ class App extends Component {
     }));
     ContactsAPI.remove(contact);
   };
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([contact])
+      }));
+    });
+  }
   render() {
     return (
       <div className="app">
@@ -57,7 +64,17 @@ class App extends Component {
             />
           )}
         />
-        <Route path="/create" component={CreateContact} />
+        <Route
+          path="/create"
+          render={({ history }) => (
+            <CreateContact
+              onCreateContact={contact => {
+                this.createContact(contact);
+                history.push('/');
+              }}
+            />
+          )}
+        />
       </div>
     );
   }
