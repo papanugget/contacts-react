173d585162c75a5a602aaf944acf54c0e7cc07c1
papanugget <evilgenius04@gmail.com>
2018-09-06 21:31:07 -0400

    added functional components and state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
+// import React from 'react';
+import ListContacts from './ListContacts';
+
+// const contacts = [{
+//         "id": "ryan",
+//         "name": "Ryan Florence",
+//         "email": "ryan@reacttraining.com",
+//         "avatarURL": "http://localhost:5001/ryan.jpg"
+//     },
+//     {
+//         "id": "michael",
+//         "name": "Michael Jackson",
+//         "email": "michael@reacttraining.com",
+//         "avatarURL": "http://localhost:5001/michael.jpg"
+//     },
+//     {
+//         "id": "tyler",
+//         "name": "Tyler McGinnis",
+//         "email": "tyler@reacttraining.com",
+//         "avatarURL": "http://localhost:5001/tyler.jpg"
+//     }
+// ];
 
 class App extends Component {
-    render() {
-        return ( < div >
-            Hello World
-
-            <
-            /div >
-        )
-    }
+  state = {
+    contacts: [
+      {
+        id: 'ryan',
+        name: 'Ryan Florence',
+        email: 'ryan@reacttraining.com',
+        avatarURL: 'http://localhost:5001/ryan.jpg'
+      },
+      {
+        id: 'michael',
+        name: 'Michael Jackson',
+        email: 'michael@reacttraining.com',
+        avatarURL: 'http://localhost:5001/michael.jpg'
+      },
+      {
+        id: 'tyler',
+        name: 'Tyler McGinnis',
+        email: 'tyler@reacttraining.com',
+        avatarURL: 'http://localhost:5001/tyler.jpg'
+      }
+    ]
+  };
+  removeContact = contact => {
+    this.setState(state => ({
+      contacts: state.contacts.filter(c => c.id !== contact.id)
+    }));
+  };
+  render() {
+    return (
+      <ListContacts
+        onDeleteContact={this.removeContact}
+        contacts={this.state.contacts}
+      />
+    );
+  }
 }
 
 export default App;
